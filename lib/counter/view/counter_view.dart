4c4fb0b822d267ea775009773e35332884677189
DevYuns <ydhun93@gmail.com>
2020-11-08 19:25:15 +0900

    Apply basic bloc pattern

---
+++ lib/counter/view/counter_view.dart
@@ -0,0 +1,46 @@
+import 'package:bloc_practice/counter/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../counter.dart';
+
+class CounterView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter'),
+      ),
+      body: Center(
+        child: BlocBuilder<CounterCubit, int>(
+          builder: (context, state) {
+            return Text(
+              '$state',
+              style: textTheme.headline2,
+            );
+          },
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            key: const Key('counterView_increment_floatingActionButton'),
+            child: const Icon(Icons.add),
+            onPressed: () => context.read<CounterCubit>().increment(),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          FloatingActionButton(
+            key: const Key('counterView_dncrement_floatingActionButton'),
+            child: const Icon(Icons.remove),
+            onPressed: () => context.read<CounterCubit>().decrement(),
+          ),
+        ],
+      ),
+    );
+  }
+}
